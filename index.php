7427c76326c46c7fe6735c906d1d5b8a98434948
iaio998 <depalatisiacopo@gmail.com>
2023-11-28 14:14:11 +0100

    Snack 1 completed

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+$arrayTeam = [
+    [
+        'teamHome' => 'L.A. Clippers',
+        'teamAway' => 'L.A. Lakers',
+        'scoreHome' => 76,
+        'scoreAway' => 62
+    ],
+    [
+        'teamHome' => 'N.Y Knights',
+        'teamAway' => 'Phoenix Suns',
+        'scoreHome' => 58,
+        'scoreAway' => 82
+    ],
+    [
+        'teamHome' => 'Boston Celtics',
+        'teamAway' => 'Toronto Raptors',
+        'scoreHome' => 103,
+        'scoreAway' => 104
+    ]
+]
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,7 +44,19 @@
 <body>
 
     <main>
-
+        <h1>SNACK 1: BASKETBALL TEAMS</h1>
+        <ul>
+            <?php
+            foreach ($arrayTeam as $key => $value) {
+                echo "<li>{$value['teamHome']} - {$value['teamAway']} | {$value['scoreHome']}-{$value['scoreAway']}</li>"
+                    ?>
+            <?php } ?>
+        </ul>
+        <!-- <form action="info.php" class="p-5">
+            <textarea rows="4" cols="50" name="testo" placeholder="Inserisci un testo..."></textarea>
+            <input type="text" name="parola" placeholder="Scrivi...">
+            <button type="submit">Invia</button>
+        </form> -->
     </main>
 
 
